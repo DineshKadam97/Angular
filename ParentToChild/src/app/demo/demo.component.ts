d03b114bf2b3a8617713e4cd27fd8ca48099a3de
DineshKadam97 <132736142+DineshKadam97@users.noreply.github.com>
2024-10-21 11:35:51 +0530

    Add files via upload

---
+++ ParentToChild/src/app/demo/demo.component.ts
@@ -0,0 +1,16 @@
+import { Component } from '@angular/core';
+import { Input } from '@angular/core';
+
+@Component({
+  selector: 'app-demo',
+  standalone: true,
+  imports: [],
+  templateUrl: './demo.component.html',
+  styleUrl: './demo.component.css'
+})
+
+export class DemoComponent 
+{
+  @Input() public parentData : any;
+
+}
